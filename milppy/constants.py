d55ae4f05fdab5e55b6398a55547b8a276460322
Túlio Toffolo <tulio@toffolo.com.br>
2018-11-12 00:18:04 -0200

    Creating (temporary) constants.py file

---
+++ milppy/constants.py
@@ -0,0 +1,36 @@
+# epsilon number (practical zero)
+EPS = 10e-6
+
+# infinity representation
+INF = float("inf")
+
+# optimization status
+ERROR = -1
+OPTIMAL = 0
+INFEASIBLE = 1
+UNBOUNDED = 2
+FEASIBLE = 3
+INT_INFEASIBLE = 4
+NO_SOLUTION_FOUND = 5
+LOADED = 6
+CUTOFF = 7
+
+# constraint senses
+EQUAL = "="
+LESS_OR_EQUAL = "<"
+GREATER_OR_EQUAL = ">"
+
+# optimization directions
+MINIMIZE = "MIN"
+MAXIMIZE = "MAX"
+
+# variable types
+BINARY = "B"
+CONTINUOUS = "C"
+INTEGER = "I"
+
+# solvers
+CBC = "CBC"
+CPLEX = "CPX"
+GUROBI = "GRB"
+SCIP = "SCIP"
