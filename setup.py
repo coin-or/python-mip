690547e2b9650b5b0fcebe2c9bdcb3473fc3cd0b
Haroldo <haroldo.santos@gmail.com>
2018-11-28 22:08:37 +0100

    setup pip
    Former-commit-id: 51c32f73f6c4946606bcc1328f5df016e1af2dfc

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mip",
+    version="1.0.1",
+    author="Toffolo, T.A.M. and Santos, H.G.",
+    author_email="haroldo@ufop.edu.br",
+    description="Python tools for Modeling and Solving Mixed-Integer Programs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuliotoffolo/python-mip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
