987009031d3bf1dfefc2783bee6f4f1cfafe392d
Haroldo Gambini Santos <haroldo@ufop.edu.br>
2018-12-19 14:33:17 -0200

    sphinx ext

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ release = ''
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-	'sphinxcontrib.napoleon'
+	'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
