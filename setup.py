c53994676cb776d8d8f7a2601d5f4123eafd12d1
Haroldo <haroldo.santos@gmail.com>
2019-01-01 20:18:11 -0200

    mipstart in CBC

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mip",
-    version="1.0.14",
+    version="1.0.15",
     author="Toffolo, T.A.M. and Santos, H.G.",
     author_email="haroldo@ufop.edu.br",
     description="Python tools for Modeling and Solving Mixed-Integer Programs",
