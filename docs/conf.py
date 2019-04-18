82184c1c1ca7ef276bc9a550ea47bb3dafd6c820
Haroldo <haroldo.santos@gmail.com>
2019-04-18 13:52:04 +0200

    more doc
    Former-commit-id: 488acfed6c936031c36c2fae02aa26c8eb2eca56

--- docs/conf.py
+++ docs/conf.py
@@ -109,8 +109,6 @@ htmlhelp_basename = 'python-mipdoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_engine = 'xelatex'
-
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
