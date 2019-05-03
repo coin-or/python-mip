482a644db6da519c27efb9c5e4a9fa7787eeee7a
Túlio Toffolo <tulio@toffolo.com.br>
2019-05-03 11:40:21 -0300

    Minor PEP8 modifications.
    Former-commit-id: 528ef278c905d62ca1b3fae08ca3305422156bdd

--- mip/constants.py
+++ mip/constants.py
@@ -1,21 +1,12 @@
 VERSION = '1.0.29'
 
+from enum import Enum
+
 # epsilon number (practical zero)
 EPS = 10e-6
 
 # infinity representation
 INF = float("inf")
-
-# optimization status
-ERROR = -1
-OPTIMAL = 0
-INFEASIBLE = 1
-UNBOUNDED = 2
-FEASIBLE = 3
-INT_INFEASIBLE = 4
-NO_SOLUTION_FOUND = 5
-LOADED = 6
-CUTOFF = 7
 
 # constraint senses
 EQUAL = "="
@@ -23,20 +14,39 @@ LESS_OR_EQUAL = "<"
 GREATER_OR_EQUAL = ">"
 
 # optimization directions
+MIN = "MIN"
+MAX = "MAX"
 MINIMIZE = "MIN"
 MAXIMIZE = "MAX"
 
-# Search emphasis
-FEASIBILITY = 1
-OPTIMALITY = 2
+# solvers
+CBC = "CBC"
+CPX = "CPX"
+CPLEX = "CPX"
+GRB = "GRB"
+GUROBI = "GRB"
+SCIP = "SCIP"
 
 # variable types
 BINARY = "B"
 CONTINUOUS = "C"
 INTEGER = "I"
 
-# solvers
-CBC = "CBC"
-CPLEX = "CPX"
-GUROBI = "GRB"
-SCIP = "SCIP"
+
+# optimization status
+class OptimizationStatus(Enum):
+    ERROR = -1
+    OPTIMAL = 0
+    INFEASIBLE = 1
+    UNBOUNDED = 2
+    FEASIBLE = 3
+    INT_INFEASIBLE = 4
+    NO_SOLUTION_FOUND = 5
+    LOADED = 6
+    CUTOFF = 7
+
+
+# search emphasis
+class SearchEmphasis(Enum):
+    FEASIBILITY = 1
+    OPTIMALITY = 2
