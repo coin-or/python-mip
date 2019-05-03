02238a37c2811aa1831e4d68550227ea4598ded9
Túlio Toffolo <tulio@toffolo.com.br>
2019-05-03 15:46:05 -0300

    - Creating function Model.clear() - Updating CBC to comply with SearchEmphasis Enum.
    Former-commit-id: 01e0b0a77bc1c5f081eed829c9474ce83a1483c8 [formerly d39fd5393c682fb3da3581d56032dbad532bbd4a]
    Former-commit-id: 3ee9a7ec315ab0d073b69fc3fd5b9d4c221024a4

--- examples/queens.py
+++ examples/queens.py
@@ -5,23 +5,23 @@ from time import process_time
 n = 10
 
 # can force a solver to be used with -solver=solverName option
-solver = ""
+solver = CBC
 
 for arg in argv:
     if "-solver=" in arg:
         solver = arg.split('=')[1]
         print('trying to load {} solver'.format(solver))
 
-if (len(solver)) == 0:
-    queens = Model('queens', MINIMIZE)
+if not solver:
+    queens = Model('queens', MAXIMIZE)
 else:
-    queens = Model('queens', MINIMIZE, solver)
+    queens = Model('queens', MAXIMIZE, solver)
 
 x = [[queens.add_var('x({},{})'.format(i, j), var_type='B')
       for j in range(n)] for i in range(n)]
 
 # objective function
-queens += xsum(-x[i][j] for i in range(n) for j in range(n))
+queens += xsum(x[i][j] for i in range(n) for j in range(n))
 
 # one per row
 for i in range(n):
@@ -39,16 +39,18 @@ for p, k in enumerate(range(2 - n, n - 2 + 1)):
 for p, k in enumerate(range(3, n + n)):
     queens += xsum(x[i][j] for i in range(n) for j in range(n) if i + j == k) <= 1, 'diag2({})'.format(p)
 
+queens.read('queens.lp')
 queens.optimize()
 
-for i in range(n):
-    for j in range(n):
-        if x[i][j].x >= 0.98:
-            stdout.write(' O')
-        else:
-            stdout.write(' .')
-    stdout.write('\n')
+stdout.write('\n')
+for i, v in enumerate(queens.vars):
+    if v.x >= 0.98:
+        stdout.write('O ')
+    else:
+        stdout.write('. ')
 
+    if i % n == n-1:
+        stdout.write('\n')
 stdout.write('\n')
 
 stdout.write('Total process time: {:.3f}s\n'.format(process_time()))
