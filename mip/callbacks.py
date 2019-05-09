8e8d3acd87ed42f435c72c740c388d994706ee89
Haroldo <haroldo.santos@gmail.com>
2019-05-09 19:33:40 +0200

    move callbacks to callbacks.py

--- mip/callbacks.py
+++ mip/callbacks.py
@@ -11,25 +11,46 @@ class BranchSelector:
         raise NotImplementedError()
 
 
+class CutsGenerator:
+    """abstract class for implementing cut generators"""
+    def __init__(self, model: "Model"):
+        self.model = model
+
+    def generate_cuts(self, rsol: List[Tuple["Var", float]]) -> List["LinExpr"]:
+        """Method called by the solve engine to generate cuts
+
+           After analyzing the contents of the fractional solution in :code:`relax_solution`, one
+           or mode cuts (:class:`~mip.model.LinExpr`) may be generated and returned. These cuts are added to the
+           relaxed model.
+
+        Args:
+            rsol(List[Tuple[Var, float]]): a list of tuples (variable,value) indicating the values of variables in the current fractional solution. Variables at zero are not included.
+
+        Note: take care not to query the value of the fractional solution in the cut generation method using the :code:`x`
+        methods from original references to problem variables, use the contents of :code:`relax_solution` instead.
+        """
+        raise NotImplementedError()
+
+
 class CutPool:
-    def __init__(self):
+    def __init__(self : "CutPool"):
         """Stores a list list of different cuts, repeated cuts are discarded.
         """
         self.__cuts = []
 
         # positions for each hash code to speedup
         # the search of repeated cuts
-        self.__pos = defaultdict(list)
+        self.__pos = defaultdict( list )
 
-    def add(self, cut: "LinExpr") -> bool:
+    def add(self : "CutPool", cut : "LinExpr") -> bool:
         """tries to add a cut to the pool, returns true if this is a new cut, false if it is a repeated one
 
         Args:
             cut(LinExpr): a constraint
         """
         hcode = hash(cut)
-        bucket = self.__pos[hcode]
-        for p in bucket:
+        l = self.__pos[hcode]
+        for p in l:
             if self.__cuts[p].equals(cut):
                 return False
 
@@ -39,31 +60,8 @@ class CutPool:
         return True
 
     @property
-    def cuts(self) -> List["LinExpr"]:
+    def cuts(self: "CutPool") -> List["LinExpr"]:
         return self.__cuts
-
-
-class CutsGenerator:
-    """abstract class for implementing cut generators"""
-
-    def __init__(self, model: "Model"):
-        self.model = model
-
-    def generate_cuts(self, relax_solution: List[Tuple[str, float]]) -> List["LinExpr"]:
-        """Method called by the solve engine to generate cuts
-
-           After analyzing the contents of the fractional solution in :code:`relax_solution`, one
-           or mode cuts (:class:`~mip.model.LinExpr`) may be generated and returned. These cuts are added to the
-           relaxed model.
-
-        Args:
-            relax_solution(List[Tuple[str, float]]): a list of tuples (str,value) indicating the values
-            of variables in the current fractional solution. Variables at zero are not included.
-
-        Note: take care not to query the value of the fractional solution in the cut generation method using the :code:`x`
-        methods from original references to problem variables, use the contents of :code:`relax_solution` instead.
-        """
-        raise NotImplementedError()
 
 
 class IncumbentUpdater:
