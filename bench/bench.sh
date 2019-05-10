e2ce9da0d62dfbce0d64c5f6c1f85c935d5cbb83
Haroldo <haroldo.santos@gmail.com>
2019-05-10 13:10:49 +0200

    updated benchmark scripts

---
+++ bench/bench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -f *.csv
+
+python3 queens.py cbc
+mv queens-mip-cbc.csv queens-mip-cbc-cpython.csv
+
+python3 queens.py gurobi
+mv queens-mip-gurobi.csv queens-mip-gurobi-cpython.csv
+
+pypy3 queens.py cbc
+mv queens-mip-cbc.csv queens-mip-cbc-pypy.csv
+
+python3 queens-gurobi.py
+
+python3 queens-pulp.py
+mv queens-pulp.csv queens-pulp-cpython.csv
+
+pypy3 queens-pulp.py
+mv queens-pulp.csv queens-pulp-pypy.csv
+
