c3aef234819a5d136a4453f7c920f0672899b177
Haroldo <haroldo.santos@gmail.com>
2019-05-10 12:17:40 +0000

    bench
    Former-commit-id: 873ff5d5d1404ecc42d6a117c6427bb09732dba1 [formerly bbeda0d10a7d15b1e091ecb3d2d33399fb743a77]
    Former-commit-id: 79ac46c0c45c42101960be03c6c6b969605710b4

--- bench/bench.sh
+++ bench/bench.sh
@@ -11,6 +11,9 @@ mv queens-mip-gurobi.csv queens-mip-gurobi-cpython.csv
 pypy3 queens.py cbc
 mv queens-mip-cbc.csv queens-mip-cbc-pypy.csv
 
+pypy3 queens.py gurobi
+mv queens-mip-gurobi.csv queens-mip-gurobi-pypy.csv
+
 python3 queens-gurobi.py
 
 python3 queens-pulp.py
