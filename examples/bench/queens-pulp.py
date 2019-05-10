e8dbb2d7008f1ad358c7966c631bead487191fff
Haroldo <haroldo.santos@gmail.com>
2019-05-10 11:20:33 +0200

    updated benchmark scripts
    Former-commit-id: 7a234e1f19d7bc5b278f724f0ac0dec8b26096ea [formerly ffdd4559db36d8016464d3fe9661bbce7ec2de8c]
    Former-commit-id: 6a2f245449549dc72cd513320b602b0126556b91

--- examples/bench/queens-pulp.py
+++ examples/bench/queens-pulp.py
@@ -4,43 +4,60 @@ from time import process_time
 import time
 import timeout_decorator
 
-N = range(100,1001,100)
+N = range(100, 1001, 100)
 
-f=open('queens-pulp.csv', 'w')
+f = open('queens-pulp.csv', 'w')
 
-@timeout_decorator.timeout(1000)
-def gen_model( n ):
-	st = time.time()
-	queens = LpProblem('queens', LpMinimize)
+TIMEOUT = 1000
+execTime = TIMEOUT
+modelCols = 0
+modelRows = 0
+modelNz = 0
 
-	x = [[LpVariable('x({},{})'.format(i, j), 0, 1, 'Binary')
-		  for j in range(n)] for i in range(n)]
 
-	# objective function
-	queens += lpSum(-x[i][j] for i in range(n) for j in range(n))
+@timeout_decorator.timeout(TIMEOUT)
+def gen_model(n):
+    global execTime
+    global modelCols
+    global modelRows
+    global modelNz
+    execTime = TIMEOUT
+    modelCols = 0
+    modelRows = 0
+    modelNz = 0
+    st = time.time()
+    queens = LpProblem('queens', LpMinimize)
 
-	# one per row
-	for i in range(n):
-		queens += lpSum(x[i][j] for j in range(n)) == 1, 'row({})'.format(i)
+    x = [[LpVariable('x({},{})'.format(i, j), 0, 1, 'Binary')
+          for j in range(n)] for i in range(n)]
 
-	# one per column
-	for j in range(n):
-		queens += lpSum(x[i][j] for i in range(n)) == 1, 'col({})'.format(j)
+    # one per row
+    for i in range(n):
+        queens += lpSum(x[i][j] for j in range(n)) == 1, 'row({})'.format(i)
 
-	# diagonal \
-	for p, k in enumerate(range(2 - n, n - 2 + 1)):
-		queens += lpSum(x[i][j] for i in range(n) for j in range(n) if i - j == k) <= 1, 'diag1({})'.format(p)
+    # one per column
+    for j in range(n):
+        queens += lpSum(x[i][j] for i in range(n)) == 1, 'col({})'.format(j)
 
-	# diagonal /
-	for p, k in enumerate(range(3, n + n)):
-		queens += lpSum(x[i][j] for i in range(n) for j in range(n) if i + j == k) <= 1, 'diag2({})'.format(p)
+    # diagonal \
+    for p, k in enumerate(range(2 - n, n - 2 + 1)):
+        queens += lpSum(x[i][j] for i in range(n) for j in range(n)
+                        if i - j == k) <= 1, 'diag1({})'.format(p)
 
-	ed = time.time()
+    # diagonal /
+    for p, k in enumerate(range(3, n + n)):
+        queens += lpSum(x[i][j] for i in range(n) for j in range(n)
+                        if i + j == k) <= 1, 'diag2({})'.format(p)
 
-	f.write('{},{},{:.4f}\n'.format(n, 'pulp', ed-st))
-	f.flush()
+    ed = time.time()
+    execTime = ed-st
+    modelCols = queens.numVariables()
+    modelRows = queens.numConstraints()
+
 
 for n in N:
-	gen_model(n)
+    gen_model(n)
+    f.write('{},{},{:.4f}\n'.format(n, 'pulp', execTime))
+    f.flush()
 
 f.close()
