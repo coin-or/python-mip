d084417a15cefc43a405cd23ae1978cafe9993b2
Haroldo <haroldo.santos@gmail.com>
2019-06-09 19:49:03 +0200

    jssp example
    Former-commit-id: 3b4af902151c0e56306aaeee70cd833bfd3bf898

--- examples/jssp.py
+++ examples/jssp.py
@@ -1,50 +1,44 @@
+"""Job Shop Scheduling Problem Python-MIP exaxmple
+   To execute it on the example instance ft03.jssp call
+   python jssp.py ft03.jssp
+   by Victor Silva"""
+
+from itertools import product
 from sys import argv
-from JSSPInstance import JSSPInstance
+from jssp_instance import JSSPInstance
 from mip.model import Model
-from mip.constants import INTEGER, BINARY
+from mip.constants import BINARY
 
 inst = JSSPInstance(argv[1])
-print("Machines: ", inst.m, "Jobs: ", inst.n)
-print("Order: ")
-for j in range(inst.n):
-    print("Job ",j+1,": ", end='')
-    for i in range(inst.m):
-        print("machine",inst.machines[j][i]+1, "(", inst.times[j][inst.machines[j][i]], ") ", end='')
-    print()
+n, m, machines, times, M = inst.n, inst.m, inst.machines, inst.times, inst.M
 
-print("M: ",inst.M)
+model = Model('JSSP')
 
-model = Model('jssp')
-
-c = model.add_var(var_type=INTEGER, name="C")
-x = [[model.add_var(var_type=INTEGER, name='x({},{})'.format(j+1,i+1)) for i in range(inst.m)] for j in range(inst.n)]
-y = [[[model.add_var(var_type=BINARY, name='y({},{},{})'.format(j+1,k+1,i+1)) for i in range(inst.m)] for k in range(inst.n)] for j in range(inst.n)]
+c = model.add_var(name="C")
+x = [[model.add_var(name='x({},{})'.format(j+1, i+1))
+      for i in range(m)] for j in range(n)]
+y = [[[model.add_var(var_type=BINARY, name='y({},{},{})'.format(j+1, k+1, i+1))
+       for i in range(m)] for k in range(n)] for j in range(n)]
 
 model.objective = c
 
-#constraints (2)
-for j in range(inst.n):
-    for i in range(1,inst.m):
-        model += x[j][inst.machines[j][i]] - x[j][inst.machines[j][i-1]] >= inst.times[j][inst.machines[j][i-1]] , 'ord({},{})'.format(j+1,i+1)
+for (j, i) in product(range(n), range(1, m)):
+    model += x[j][machines[j][i]] - x[j][machines[j][i-1]] >= \
+        times[j][machines[j][i-1]]
 
-#constraints (3-4)
-for j in range(inst.n):
-    for k in range(inst.n):
-        if k != j:
-            for i in range(inst.m):
-                model += x[j][i] - x[k][i] + inst.M*y[j][k][i] >= inst.times[k][i], 'phi({},{},{})'.format(j+1,k+1,i+1)
-                model += -x[j][i] + x[k][i] - inst.M*y[j][k][i] >= inst.times[j][i] - inst.M, 'psy({},{},{})'.format(j+1,k+1,i+1)
-            
-#constraints (5)
-for j in range(inst.n):
-    model += c - x[j][inst.machines[j][inst.m - 1]] >= inst.times[j][inst.machines[j][inst.m - 1]], 'makespan({})'.format(j+1)
+for (j, k) in product(range(n), range(n)):
+    if k != j:
+        for i in range(m):
+            model += x[j][i] - x[k][i] + M*y[j][k][i] >= times[k][i]
+            model += -x[j][i] + x[k][i] - M*y[j][k][i] >= times[j][i] - M
+
+for j in range(n):
+    model += c - x[j][machines[j][m - 1]] >= times[j][machines[j][m - 1]]
 
 model.optimize()
 
-#printing results
 print("C: ", c.x)
-for j in range(inst.n):
-    for i in range(inst.m):
-        print('x({},{}) = {} '.format(j+1,i+1,x[j][i].x), end='')
+for j in range(n):
+    for i in range(m):
+        print('x({},{}) = {} '.format(j+1, i+1, x[j][i].x), end='')
     print()
-
