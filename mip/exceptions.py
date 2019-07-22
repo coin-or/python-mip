26d941b555f5e413051139a9ad8a084129ecf0ce
Túlio Toffolo <tulio@toffolo.com.br>
2019-07-22 20:28:09 -0300

    MacOS binaries
    Former-commit-id: 6834d7cb1b41965a40ec7f7e4c55430198770c19

--- mip/exceptions.py
+++ mip/exceptions.py
@@ -4,6 +4,18 @@ class InvalidLinExpr(Exception):
     pass
 
 
+class InvalidParameter(Exception):
+    """Exception that is raised when an invalid/non-existent
+    parameter is used or set"""
+    pass
+
+
+class ParameterNotAvailable(Exception):
+    """Exception that is raised when some parameter is not
+    available for the current solver"""
+    pass
+
+
 class SolutionNotAvailable(Exception):
     """Exception that is raised when some method to query some
     solution property is used but no solution is available"""
