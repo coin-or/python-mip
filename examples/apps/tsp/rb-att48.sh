0756aa711f8b3d08d19a0a27953042ff132a6fba
Haroldo G. Santos <haroldo.santos@gmail.com>
2019-10-22 11:37:25 -0300

    test scripts
    Former-commit-id: d530144b62423ff05a5b8f15ecd7d9de10f52191 [formerly c792189b1b7e8ebed42c6c7c20811ef628db645a]
    Former-commit-id: a26973ee321ad63ccc29a5db3f1d996d34d67f69

---
+++ examples/apps/tsp/rb-att48.sh
@@ -0,0 +1,16 @@
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 1 0 > att48-GUROBI-1-1-0.log 2> att48-GUROBI-1-1-0.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 0 0 > att48-GUROBI-0-0-0.log 2> att48-GUROBI-0-0-0.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 1 1 > att48-GUROBI-0-1-1.log 2> att48-GUROBI-0-1-1.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 0 0 > att48-CBC-1-0-0.log 2> att48-CBC-1-0-0.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 1 1 > att48-CBC-1-1-1.log 2> att48-CBC-1-1-1.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 0 1 > att48-CBC-1-0-1.log 2> att48-CBC-1-0-1.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 0 0 > att48-CBC-0-0-0.log 2> att48-CBC-0-0-0.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 0 0 > att48-GUROBI-1-0-0.log 2> att48-GUROBI-1-0-0.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 1 1 > att48-GUROBI-1-1-1.log 2> att48-GUROBI-1-1-1.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 0 1 > att48-GUROBI-1-0-1.log 2> att48-GUROBI-1-0-1.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 0 1 > att48-GUROBI-0-0-1.log 2> att48-GUROBI-0-0-1.error
+export SOLVER_NAME=GUROBI ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 1 0 > att48-GUROBI-0-1-0.log 2> att48-GUROBI-0-1-0.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 1 1 > att48-CBC-0-1-1.log 2> att48-CBC-0-1-1.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 1 1 0 > att48-CBC-1-1-0.log 2> att48-CBC-1-1-0.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 1 0 > att48-CBC-0-1-0.log 2> att48-CBC-0-1-0.error
+export SOLVER_NAME=CBC ; pypy3 tsp.py /home/haroldo/inst/tsp//att48.tsp 36000 1 0 0 1 > att48-CBC-0-0-1.log 2> att48-CBC-0-0-1.error
