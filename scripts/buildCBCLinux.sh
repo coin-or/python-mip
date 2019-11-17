2c7887ae20d70166ac14ec7d49e78f7ccebedd72
Haroldo G. Santos <haroldo.santos@gmail.com>
2019-11-17 19:48:34 -0300

    new linux binary, starting to add LP method configurability
    Former-commit-id: 2108a0fa61b6d9e601b96741d7fe653f04b2561f

--- scripts/buildCBCLinux.sh
+++ scripts/buildCBCLinux.sh
@@ -16,12 +16,5 @@ export LDFLAGS="-Ofast -fPIC -L/opt/gcc/lib -flto -static-libgcc -static-libstdc
 ./configure --prefix=~/prog/ --enable-cbc-parallel --enable-static --disable-shared 
 make clean 
 make -j 2
-g++ -shared -Ofast -fPIC -o cbc-c-linux-x86-64.so \
--I~/prog/include/ -I./CoinUtils/src/ -I./Osi/src/ -I./Clp/src/ -I./Clp/src/ \
--I./Cgl/src/ -I./Cbc/src/ -I./Osi/src/Osi/ -I./Clp/src/OsiClp/ -DCBC_THREAD \
-./Cbc/src/Cbc_C_Interface.cpp -L/opt/gcc/lib64/ -L./Cbc/src/.libs/ -L./Cgl/src/.libs/ \
--L./Cgl/src/.libs/ -L./Osi/src/Osi/.libs/ -L./Clp/src/OsiClp/.libs/ -L./Clp/src/.libs/ \
--L./CoinUtils/src/.libs/ -L./Cgl/src/.libs/ -L./Clp/src/OsiClp/.libs/ -L./Clp/src/.libs/ \
--L./ThirdParty/Lapack/.libs/ -L./ThirdParty/Blas/.libs/ \
- -lCbcSolver -lCbc -lpthread -lrt -lCgl -lOsiClp -lClpSolver -lClp -lOsi -lCoinUtils \
- -lcoinlapack -lcoinblas -lgfortran -lquadmath -lm -static-libgcc -static-libstdc++ -static-libgfortran
+
+g++ -shared -Ofast -fPIC -o cbc-c-linux-x86-64.so -I~/prog/include/ -I./CoinUtils/src/ -I./Osi/src/ -I./Clp/src/ -I./Clp/src/ -I./Cgl/src/ -I./Cbc/src/ -I./Osi/src/Osi/ -I./Clp/src/OsiClp/ -DCBC_THREAD ./Cbc/src/Cbc_C_Interface.cpp -L/opt/gcc/lib64/ -L./Cbc/src/.libs/ -L./Cgl/src/.libs/ -L./Cgl/src/.libs/ -L./Osi/src/Osi/.libs/ -L./Clp/src/OsiClp/.libs/ -L./Clp/src/.libs/ -L./CoinUtils/src/.libs/ -L./Cgl/src/.libs/ -L./Clp/src/OsiClp/.libs/ -L./Clp/src/.libs/ -L./ThirdParty/Lapack/.libs/ -L./ThirdParty/Blas/.libs/ -L./ThirdParty/Glpk/.libs/  -lCbcSolver -lCbc -lpthread -lrt -lCgl -lOsiClp -lClpSolver -lClp -lOsi -lCoinUtils  -lcoinlapack -lcoinblas -lgfortran -lquadmath -lm -static-libgcc -static-libstdc++ -static-libgfortran -lcoinglpk
