f1fa63c9007ab1b73d00aba800f3cf3130730ab3
tommyod <tommy.odland@gmail.com>
2019-12-19 14:57:36 +0100

    added a doctest as an example
    Former-commit-id: b84dca933be9589f8323ed0f82a4a4335613e1b2

--- mip/model.py
+++ mip/model.py
@@ -25,6 +25,17 @@ class Model:
     Attributes:
         vars(VarList): list of problem variables (:class:`~mip.model.Var`)
         constrs(ConstrList): list of constraints (:class:`~mip.model.Constr`)
+        
+    Examples:
+        >>> from mip import Model, MAXIMIZE, CBC, INTEGER, OptimizationStatus
+        >>> model = Model(sense=MAXIMIZE, solver_name=CBC)
+        >>> x = model.add_var(name='x', var_type=INTEGER, lb=0, ub=10)
+        >>> y = model.add_var(name='y', var_type=INTEGER, lb=0, ub=10)
+        >>> model += x + y <= 10
+        >>> model.objective = x + y
+        >>> status = model.optimize(max_seconds=2)
+        >>> status == OptimizationStatus.OPTIMAL
+        True
     """
 
     def __init__(self, name: str = "",
