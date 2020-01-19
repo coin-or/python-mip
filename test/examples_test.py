e0b4ee95a0317cddb682a4caf5b58848bf584a72
Haroldo <haroldo.santos@gmail.com>
2020-01-19 10:30:34 -0300

    automated test of all examples

---
+++ test/examples_test.py
@@ -0,0 +1,21 @@
+"""Tests all examples in the ../examples folder
+with all the solvers available"""
+from glob import glob
+from os.path import join
+from os import environ
+from itertools import product
+import imp
+import pytest
+
+
+EXAMPLES = glob(join("..", "examples", "*.py"))
+
+SOLVERS = ["cbc"]
+if "GUROBI_HOME" in environ:
+    SOLVERS += ["gurobi"]
+
+
+@pytest.mark.parametrize("solver, example", product(SOLVERS, EXAMPLES))
+def test_examples(solver, example):
+    environ["SOLVER_NAME"] = solver
+    m = imp.load_source("a", example)
