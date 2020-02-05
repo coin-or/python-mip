cd4cc67fb731767e2bd45bddc6402d3911822f46
Haroldo <haroldo.santos@gmail.com>
2020-02-05 13:36:30 -0300

    scripts to build cbc using the new organization
    Former-commit-id: 763a95a56d9c3cc105066b057432f4595e348ca9

---
+++ scripts/downloadCBC.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/coin-or/CoinUtils
+git clone https://github.com/coin-or/Osi
+git clone https://github.com/coin-or/Clp
+git clone https://github.com/coin-or/Cgl
+git clone https://github.com/coin-or/Cbc
+
+git clone https://github.com/coin-or-tools/ThirdParty-Glpk.git
+cd ThirdParty-Glpk
+./get.Glpk
+cd ../
+
+git clone https://github.com/coin-or-tools/ThirdParty-Lapack.git
+cd ThirdParty-Lapack
+./get.Lapack
+cd ..
+
+git clone https://github.com/coin-or-tools/ThirdParty-Blas.git
+cd ThirdParty-Blas
+./get.Blas
+cd ..
