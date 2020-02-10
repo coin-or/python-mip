720dbd59b2681d6091bd6fa34cf01223f21d7442
Haroldo <haroldo.santos@gmail.com>
2020-02-10 16:01:30 -0300

    scripts for downloading and building Cbc from svn and git
    Former-commit-id: 4ba32ec1d68cfce49d0b1e9ca2d6c5a87b44d23e

---
+++ scripts/buildCBCLinuxAs.sh
@@ -0,0 +1,54 @@
+export CFLAGS="-Og -fPIC -g -fsanitize=address"
+export FFLAGS="-Og -fPIC -g -fsanitize=address"
+export CXXFLAGS="-Og -fPIC -g -fsanitize=address"
+export LDFLAGS="-Og -fPIC -g -static-libgcc -static-libstdc++ -fsanitize=address"
+
+dir=`pwd`
+mkdir -p ~/prog
+cd ~/prog
+IDIR=`pwd`
+export PKG_CONFIG_PATH=${IDIR}/lib/pkgconfig/:${PKG_CONFIG_PATH}
+
+cd $dir/ThirdParty-Glpk
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared
+make -j 6
+make -j 6 install
+
+
+cd $dir/ThirdParty-Lapack
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+cd $dir/ThirdParty-Blas
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+cd $dir/CoinUtils
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+cd $dir/Osi
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+cd $dir/Clp
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+cd $dir/Cgl
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+
+cd $dir/Cbc
+./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas --enable-static --disable-shared 
+make -j 6
+make -j 6 install
+
+cd $dir
