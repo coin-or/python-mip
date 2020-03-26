404282c470eedb7472b3ac27c892c6906187d695
Haroldo <haroldo.santos@gmail.com>
2020-03-26 13:26:34 -0300

    script to download cbc and dependencies

---
+++ scripts/downloadCBCsvn.sh
@@ -0,0 +1,91 @@
+echo "Checking CoinUtils"
+if [[ -d CoinUtils ]]
+then
+    cd CoinUtils
+    svn up
+    cd ..
+else
+    svn co https://projects.coin-or.org/svn/CoinUtils/trunk/ CoinUtils
+fi
+
+echo "Checking Osi"
+if [[ -d Osi ]]
+then
+    cd Osi
+    svn up
+    cd ..
+else
+    svn co https://projects.coin-or.org/svn/Osi/trunk/ Osi
+fi
+
+echo "Checking Clp"
+if [[ -d Clp ]]
+then
+    cd Clp
+    svn up
+    cd ..
+else
+    svn co https://projects.coin-or.org/svn/Clp/trunk/ Clp
+fi
+
+echo "Checking Cgl"
+if [[ -d Cgl ]]
+then
+    cd Cgl
+    svn up
+    cd ..
+else
+    svn co https://projects.coin-or.org/svn/Cgl/trunk/ Cgl
+fi
+
+echo "Checking Cbc"
+if [[ -d Cbc ]]
+then
+    cd Cbc
+    svn up
+    cd ..
+else
+    svn co https://projects.coin-or.org/svn/Cbc/trunk/ Cbc
+fi
+
+echo "Checking Glpk"
+if [[ -d ThirdParty-Glpk ]]
+then
+    cd ThirdParty-Glpk
+    git pull
+    ./get.Glpk
+    cd ..
+else
+    git clone https://github.com/coin-or-tools/ThirdParty-Glpk.git
+    cd ThirdParty-Glpk
+    ./get.Glpk
+    cd ../
+fi
+
+echo "Checking Lapack"
+if [[ -d ThirdParty-Lapack ]]
+then
+    cd ThirdParty-Lapack
+    git pull
+    ./get.Lapack
+    cd ..
+else
+    git clone https://github.com/coin-or-tools/ThirdParty-Lapack.git
+    cd ThirdParty-Lapack
+    ./get.Lapack
+    cd ../
+fi
+
+echo "Checking Blas"
+if [[ -d ThirdParty-Blas ]]
+then
+    cd ThirdParty-Blas
+    git pull
+    ./get.Blas
+    cd ..
+else
+    git clone https://github.com/coin-or-tools/ThirdParty-Blas.git
+    cd ThirdParty-Blas
+    ./get.Blas
+    cd ../
+fi
