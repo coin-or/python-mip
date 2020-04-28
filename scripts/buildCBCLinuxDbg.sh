620ac112db259dbc0ebb81fdd8c9850f223ed13d
Haroldo <haroldo.santos@gmail.com>
2020-04-28 11:47:54 -0300

    script to build debug friendly cbc

--- scripts/buildCBCLinuxDbg.sh
+++ scripts/buildCBCLinuxDbg.sh
@@ -1,7 +1,7 @@
-export CFLAGS="-Og -fPIC -g3"
+export CFLAGS="-Og -fPIC -g3 -DDEBUG"
 export FFLAGS="-Og -fPIC -g3"
-export CXXFLAGS="-Og -fPIC -g3"
-export LDFLAGS="-D_GLIBCXX_DEBUG -DDEBUGCG -Og -fPIC -g"
+export CXXFLAGS="-Og -fPIC -g3 -DDEBUG"
+export LDFLAGS="-Og -fPIC -g"
 
 dir=`pwd`
 mkdir -p ~/prog
@@ -57,5 +57,14 @@ make clean ; make distclean
 ./configure --prefix=$IDIR --without-lapack --without-glpk --without-blas  --enable-shared 
 make -j 6
 make -j 6 install
+
+
+for libfile in $IDIR/lib/*.so*;
+do
+    chrpath -r ./ $libfile
+done
+
+chrpath -r \$\ORIGIN/../lib/ $IDIR/bin/cbc
+chrpath -r \$\ORIGIN/../lib/ $IDIR/bin/clp
 
 cd $dir
