66086bf4ebeacff2020f028520fa3ad09c636be7
Sebastian Heger <heger.sebastian@gmx.de>
2022-04-09 09:32:54 +0200

    Add missing import of conflicts at mip top level and optimized imports

--- mip/__init__.py
+++ mip/__init__.py
@@ -1,12 +1,13 @@
-from mip.constants import *
-from mip.solver import Solver
 from mip.callbacks import *
-from mip.log import ProgressLog
-from mip.lists import ConstrList, VarList, VConstrList, VVarList
-from mip.exceptions import *
-from mip.ndarray import LinExprTensor
+from mip.conflict import ConflictFinder, IISFinderAlgorithm
+from mip.constants import *
 from mip.entities import Column, Constr, LinExpr, Var, ConflictGraph
+from mip.exceptions import *
+from mip.lists import ConstrList, VarList, VConstrList, VVarList
+from mip.log import ProgressLog
 from mip.model import *
+from mip.ndarray import LinExprTensor
+from mip.solver import Solver
 from mip.version import version as __version__
 
 name = "mip"
