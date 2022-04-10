99325b6ce539bf359f4d5891804ac6cd9ed2ed02
Michael Jurasovic <mjurasovic@gmail.com>
2022-04-10 12:43:07 +1000

    use pathlib to get absolute path to lp file

--- test/instability_test.py
+++ test/instability_test.py
@@ -1,9 +1,11 @@
 from mip import Model, OptimizationStatus
+from pathlib import Path
 
 
 def test_windows_instability():
     model = Model()
-    model.read("./data/windows-instability.lp")
+    file = Path(__file__).parent / "data/windows-instability.lp"
+    model.read(str(file.resolve()))
     result = model.optimize(max_seconds=300)
 
     assert result in (OptimizationStatus.OPTIMAL, OptimizationStatus.FEASIBLE)
