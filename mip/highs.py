8a556d937ae27faecc888eb8fa09d7302928a4b6
Robert Schwarz <mail@rschwarz.net>
2023-01-15 09:43:18 +0100

    can use wrapper library directly

--- mip/highs.py
+++ mip/highs.py
@@ -25,16 +25,16 @@ try:
         # try library shipped with highspy packaged
         import highspy
 
-        # HACK: find dynamic library in sibling folder
         pkg_path = os.path.dirname(highspy.__file__)
-        libs_path = f"{pkg_path}.libs"
+
         # need library matching operating system
         if "linux" in sys.platform.lower():
-            pattern = "libhighs-*.so.*"
+            pattern = "highs_bindings.*.so"
         else:
             raise NotImplementedError(f"{sys.platform} not supported!")
+
         # there should only be one match
-        [libfile] = glob.glob(os.path.join(libs_path, pattern))
+        [libfile] = glob.glob(os.path.join(pkg_path, pattern))
         logger.debug("Choosing HiGHS library {libfile} via highspy package.")
 
     highslib = ffi.dlopen(libfile)
