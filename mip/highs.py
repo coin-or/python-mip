6e000a42ec4f1731bba10067e88dd4373504b160
Robert Schwarz <mail@rschwarz.net>
2023-01-15 09:39:14 +0100

    bare minimum pair of methods to test shared library

--- mip/highs.py
+++ mip/highs.py
@@ -1,6 +1,7 @@
 "Python-MIP interface to the HiGHS solver."
 
 import glob
+import numbers
 import logging
 import os
 import os.path
@@ -42,6 +43,68 @@ except Exception as e:
     logger.error(f"An error occurred while loading the HiGHS library:\n{e}")
     has_highs = False
 
+HEADER = """
+typedef int HighsInt;
+
+void* Highs_create(void);
+void Highs_destroy(void* highs);
+HighsInt Highs_readModel(void* highs, const char* filename);
+HighsInt Highs_writeModel(void* highs, const char* filename);
+HighsInt Highs_run(void* highs);
+HighsInt Highs_getModelStatus(const void* highs);
+double Highs_getObjectiveValue(const void* highs);
+HighsInt Highs_addVar(void* highs, const double lower, const double upper);
+HighsInt Highs_addRow(
+    void* highs, const double lower, const double upper, const HighsInt num_new_nz,
+    const HighsInt* index, const double* value
+);
+HighsInt Highs_changeColIntegrality(
+    void* highs, const HighsInt col, const HighsInt integrality
+);
+HighsInt Highs_changeColCost(void* highs, const HighsInt col, const double cost);
+HighsInt Highs_changeColBounds(
+    void* highs, const HighsInt col, const double lower, const double upper
+);
+HighsInt Highs_getRowsByRange(
+    const void* highs, const HighsInt from_row, const HighsInt to_row,
+    HighsInt* num_row, double* lower, double* upper, HighsInt* num_nz,
+    HighsInt* matrix_start, HighsInt* matrix_index, double* matrix_value
+);
+"""
+
+if has_highs:
+    ffi.cdef(HEADER)
+
 
 class SolverHighs(mip.Solver):
-    pass  # TODO
+    def __init__(self, model: mip.Model, name: str, sense: str):
+        super().__init__(model, name, sense)
+
+        # Store reference to library so that it's not garbage-collected (when we
+        # just use highslib in __del__, it had already become None)?!
+        self._lib = highslib
+
+        self._model = highslib.Highs_create()
+
+    def __del__(self):
+        self._lib.Highs_destroy(self._model)
+
+    def add_var(
+        self,
+        name: str = "",
+        obj: numbers.Real = 0,
+        lb: numbers.Real = 0,
+        ub: numbers.Real = mip.INF,
+        var_type: str = mip.CONTINUOUS,
+        column: "Column" = None,
+    ):
+        pass
+
+
+# create solver for testing
+if has_highs:
+    print("have highs")
+    model = None
+    solver = SolverHighs(None, "foo_name", mip.MINIMIZE)
+else:
+    print("don't have highs")
